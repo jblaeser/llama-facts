2604d5348e1e0617a9fb358bc7293127ac1c2178
Jill Blaeser <jblaeser01@gmail.com>
2019-03-30 22:48:01 -0500

    llama fact setup

--- js/main.js
+++ js/main.js
@@ -3,12 +3,16 @@
 new Vue({
     el: "#llama-div",
     data: {
-        factOne: ""
-
-
-
+        factOne: "When one llama has an issue with another llama, it will stick its tongue out to express its displeasure. They’ll also spit on other llamas.",
+        factTwo: "Mama llamas often hum to their babies, called crias. It helps them learn to recognize each other. Llamas also hum when they're anxious, tired or just curious.",
+        factThree: "When a male is interested in a female, he'll often make a gargling noise, called an orgle. Female llamas also make clicking noises.",
+        factFour: "They are willing pack animals but only to a point. An overloaded llama will simply refuse to move. These animals often lie down on the ground and they may spit, hiss, or even kick at their owners until their burden is lessened.",
+        factFive: "Llama farmers refer to llama manure as llama beans.",
+        factSix: "Llamas make excellent guards for herds of small animals. They are very social and will ‘adopt’ a group of sheep or goats as their own herd. Then they will protect the herd by chasing off coyotes and other predators. Llamas are also smart enough to distinguish between the neighbor’s dog and a predatory coyote.",
+        factSeven: "Llamas are playing a big part in flu vaccine research. Scientists have created a nasal spray derived from several llama antibodies that targets many strains of the flu at once. That would mean that you wouldn't need a new flu shot every year and coverage would be more significant.",
+        factEight: "Like Labradors and miniature horses, there's something soothing about llamas. They can be trained as professional comforters, working as therapy animals in hospital, schools and nursing homes.",
+        factNine: "The Oregon State University College of Veterinary Medicine is using their herd of 30 llamas and alpacas to study how certain hormones affect blood sugar. Because the metabolism of llamas is very similar to that of a human diabetic, the results of this research may provide insight into human diabetes treatment.",
+        factTen: "In 2015, two llamas escaped from a petting zoo in Sun City Arizona and led authorities on a sometimes high-speed, sometimes meandering chase.",
+        chaseGif: "img/llamachase.gif",
     } 
-
-
-
 })
